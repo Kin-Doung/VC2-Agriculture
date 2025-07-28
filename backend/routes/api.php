ef4187b0eaccb34a0ddb04c02775bcce4b7b3e64
SokAn <sokan.thol@student.passerellesnumeriques.org>
2025-07-28 08:12:57 +0700

    push code to branch development

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,13 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
-=======
 use App\Http\Controllers\LandController;
 use App\Http\Controllers\Api\AuthController;
->>>>>>> 4029f82 (feature backend land: create update show and delete databasce)
 
 Route::apiResource('lands', LandController::class);
 
@@ -20,7 +15,6 @@ Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logo
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-<<<<<<< HEAD
 
 // Register
 Route::post('/register', [AuthController::class, 'register']);
@@ -32,5 +26,3 @@ Route::apiResource('categories', CategoryController::class);
 
 // Product 
 Route::apiResource("products", ProductController::class);
-=======
->>>>>>> 4029f82 (feature backend land: create update show and delete databasce)
